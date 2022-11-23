9eb18e2a3e3b998cb5bb0834be02713a8c51b374
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-11-23 19:57:56 +0500

    Shifting bootstrap to rockets.js

--- src/components/pages/Rockets/Rockets.js
+++ src/components/pages/Rockets/Rockets.js
@@ -7,6 +7,7 @@ import {
 import PropTypes from 'prop-types';
 import { buttonToggle } from '../../../redux/rockets/rockets';
 import './Rocket.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Rockets = (props) => {
   const dispatch = useDispatch();
