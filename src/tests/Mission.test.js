74953b3a307fa0bf71a481ad09037c0b1a6412e2
MohamedCK <abdulhakimm280@gmail.com>
2022-11-24 13:15:00 +0200

    - Make a snapshot test for the missions component.

---
+++ src/tests/Mission.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore';
+import Missions from '../components/pages/Missions/Missions';
+
+it('Test Mission component', () => {
+  const tree = render(
+    <Provider store={store}>
+      <Router>
+        <Missions />
+      </Router>
+    </Provider>,
+  );
+  expect(tree).toMatchSnapshot();
+});
